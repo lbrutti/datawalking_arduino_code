fe7624bea612b1f49339ce668e5b39c63ec39a25
Lorenzo Brutti <lorenzo.brutti@ekr.it>
2018-09-10 09:25:20 +0200

    upload

---
+++ ESP32_BLE_UART_Demo/ESP32_BLE_UART_Demo.ino
@@ -0,0 +1,159 @@
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   In this example rxValue is the data received (only accessible inside that function).
+   And txValue is the data to be sent, in this example just a byte incremented every second.
+*/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <BME280I2C.h>
+#include <Wire.h>
+
+#define SERIAL_BAUD 115200
+
+BME280I2C bme;    // Default : forced mode, standby time = 1000 ms
+// Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
+
+
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+float txValue = 0;
+const int readPin = 32; // Use GPIO number. See ESP32 board pinouts
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+    }
+};
+
+BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+
+float temp(NAN), hum(NAN), pres(NAN);
+char tempStr[6], humStr[6], presStr[10];
+char buffer[22];
+
+void setup() {
+  pinMode(5, OUTPUT);
+  Serial.begin(SERIAL_BAUD);
+
+  while (!Serial) {} // Wait
+
+  Wire.begin();
+
+  while (!bme.begin())
+  {
+    Serial.println("Could not find BME280 sensor!");
+    delay(1000);
+  }
+
+  // bme.chipID(); // Deprecated. See chipModel().
+  switch (bme.chipModel())
+  {
+    case BME280::ChipModel_BME280:
+      Serial.println("Found BME280 sensor! Success.");
+      break;
+    case BME280::ChipModel_BMP280:
+      Serial.println("Found BMP280 sensor! No Humidity available.");
+      break;
+    default:
+      Serial.println("Found UNKNOWN sensor! Error!");
+  }
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32 UART Test"); // Give it a name
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+  readAndStore();
+  blinkOK();
+}
+
+void readAndStore() {
+  bme.read(pres, temp, hum, tempUnit, presUnit);
+  dtostrf(temp, 5, 2, tempStr);
+  dtostrf(hum, 5, 2, humStr);
+  dtostrf(pres, 9, 2, presStr);
+  sprintf(buffer, "%s,%s,%s", tempStr, humStr, presStr);
+}
+void loop() {
+
+  if (deviceConnected) {
+    readAndStore();
+    pCharacteristic->setValue(buffer);
+
+    pCharacteristic->notify(); // Send the value to the app!
+
+    blinkOK();
+    Serial.print("*** Sent Value: ");
+    Serial.print(buffer);
+    Serial.println(" ***");
+  }
+  delay(60 * 1000);
+}
+
+void blinkOK() {
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
+  // wait for a second
+}
